b9139b407ae427f5b354b8c4f2bd125e2936d563
Apchibudzdravij <Apchibudzdravij@users.noreply.github.com>
2024-09-05 06:32:49 +0200

    First implementation of provided methods

--- src/main/java/org/example/FolderCabinet.java
+++ src/main/java/org/example/FolderCabinet.java
@@ -1,14 +1,28 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class FolderCabinet implements Cabinet, MultiFolder  {
+public class FolderCabinet implements Cabinet  {
     private List<Folder> folders;
 
-    private class FolderFromCabinet implements Folder {
+    private class FolderFromCabinet implements MultiFolder {
         private String name;
         private String size;
+
+        public FolderFromCabinet(String name, String size) throws Exception {
+            switch (size) {
+                case "SMALL":
+                case "MEDIUM":
+                case "LARGE":
+                    this.name = name;
+                    this.size = size;
+                    break;
+                default:
+                    throw new Exception("[ERROR] Incorrect SIZE parameter: " + size);
+            }
+        }
 
         public String getName() {
             return name;
@@ -16,25 +30,34 @@ public class FolderCabinet implements Cabinet, MultiFolder  {
         public String getSize() {
             return size;
         }
+
+        @Override
+        public List<Folder> getFolders() {
+            return folders;
+        }
     }
 
     @Override
     public Optional<Folder> findFolderByName(String name) {
+        for (Folder f: folders) {
+            if (f.getName().equals(name))
+                return Optional.of(f);
+        }
         return Optional.empty();
     }
 
     @Override
     public List<Folder> findFoldersBySize(String size) {
-        return List.of();
+        List<Folder> resultList = new ArrayList<Folder>();
+        for (Folder f: folders) {
+            if (f.getSize().equals(size))
+                resultList.add(f);
+        }
+        return resultList;
     }
 
     @Override
     public int count() {
-        return 0;
-    }
-
-    @Override
-    public List<Folder> getFolders() {
-        return folders;
+        return folders.size();
     }
 }
